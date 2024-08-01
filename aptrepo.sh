b7bdb078b5f317dafcf18489b4de14752297ec54
nitondev <email@niton.dev>
2024-08-01 20:04:39 +0200

    colors added

--- aptrepo.sh
+++ aptrepo.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+FG='\033[1;32m'
+FC='\033[1;36m'
+NC='\033[0m'
+
 # Define repository details
 REPO_URL="https://archive.niton.dev/apt"
 REPO_FILE="/etc/apt/sources.list.d/nitondev.list"
+
+echo -e "[${FG}+${NC}] adding ${FC}archive.niton.dev${NC} to sources"
 
 # Create the APT repository configuration file
 echo "deb [trusted=yes] ${REPO_URL} stable main" | sudo tee ${REPO_FILE} > /dev/null
@@ -10,4 +16,4 @@ echo "deb [trusted=yes] ${REPO_URL} stable main" | sudo tee ${REPO_FILE} > /dev/
 # Update APT cache
 sudo apt-get update
 
-echo "APT repository added and cache updated."
+echo -e "[${FG}+${NC}] ${FC}archive.niton.dev${NC} added and cache updated."
