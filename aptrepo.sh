96b8807ecffb8b144a8c33b89ad05f836900b91e
Lord Hackwell (niton) <email@niton.dev>
2024-11-15 23:06:54 +0100

    update(s)

--- aptrepo.sh
+++ aptrepo.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-FG='\033[1;32m'
-FC='\033[1;36m'
-NC='\033[0m'
+set -e
 
-# Define repository details
 REPO_URL="https://archive.niton.dev/apt"
-REPO_FILE="/etc/apt/sources.list.d/nitondev.list"
+KEY_URL="https://archive.niton.dev/apt/key.gpg"
+DISTRO="stable"
+COMPONENT="main"
 
-echo -e "[${FG}+${NC}] adding ${FC}archive.niton.dev${NC} to sources"
+echo "Connected to $REPO_URL"
+echo "[ + ] adding nitonsr.list"
 
-# Create the APT repository configuration file
-echo "deb [trusted=yes] ${REPO_URL} stable main" | sudo tee ${REPO_FILE} > /dev/null
+echo "deb $REPO_URL $DISTRO $COMPONENT" | sudo tee /etc/apt/sources.list.d/niton.list > /dev/null
 
-# Update APT cache
-sudo apt-get update
+echo "[ + ] downloading key.gpg"
 
-echo -e "[${FG}+${NC}] ${FC}archive.niton.dev${NC} added and cache updated."
+curl -fsSL $KEY_URL | sudo tee /etc/apt/trusted.gpg.d/niton-archive.asc > /dev/null
+
+echo "[ + ] key.gpg installed"
+
+echo "[ + ] updating apt cache"
+sudo apt-get update -y > /dev/null
+
+echo -e "\n[ + ] installation complete."
