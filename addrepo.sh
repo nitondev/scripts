a46234fde71a67fd74f87d0961c8dfd622ba0074
Lord J. Hackwell <lordhck@niton.dev>
2025-03-05 14:51:57 +0100

    added remove option

--- addrepo.sh
+++ addrepo.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-
 echo -e "[niton.dev repo install]\n"
 
-if command -v apt &> /dev/null; then
-	update_command="apt update"
-	echo "Debian-based system."
-elif command -v yum &> /dev/null; then
-	update_command="yum makecache"
-	echo "RPM Compatible system."
-elif command -v dnf &> /dev/null; then
-	update_command="dnf makecache"
-	echo "RPM Compatible system."
-else
-	echo "Didn't find supported system."
-	exit 1
-fi
+install() {
+	if command -v apt &> /dev/null; then
+		update_command="apt update"
+		echo "Debian-based system."
+	elif command -v yum &> /dev/null; then
+		update_command="yum makecache"
+		echo "RPM Compatible system."
+	elif command -v dnf &> /dev/null; then
+		update_command="dnf makecache"
+		echo "RPM Compatible system."
+	else
+		echo "Didn't find supported system."
+		exit 1
+	fi
 
 echo -e "\nRun 'sudo $update_command'"
+
+}
+
+remove() {
+	echo "Repository removed from system."
+}
+
+if [ "$1" == "remove"]; then
+	remove
+else
+	install
+fi
