6c0a059bcffd9108631313d7a20f0a9d12a3c413
Lord J. Hackwell <lordhck@niton.dev>
2025-03-05 15:28:29 +0100

    does this work?

--- addrepo.sh
+++ addrepo.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
-
 echo -e "[niton.dev repo install]\n"
 
 install() {
-	if command -v apt &> /dev/null; then
-		update_command="apt update"
-		echo "Debian-based system."
-	elif command -v yum &> /dev/null; then
-		update_command="yum makecache"
-		echo "RPM Compatible system."
-	elif command -v dnf &> /dev/null; then
-		update_command="dnf makecache"
-		echo "RPM Compatible system."
-	else
-		echo "Didn't find supported system."
-		exit 1
-	fi
-
-echo -e "\nRun 'sudo $update_command'"
-
+    if command -v apt &> /dev/null; then
+        update_command="apt update"
+        echo "Debian-based system."
+    elif command -v yum &> /dev/null; then
+        update_command="yum makecache"
+        echo "RPM Compatible system."
+    elif command -v dnf &> /dev/null; then
+        update_command="dnf makecache"
+        echo "RPM Compatible system."
+    else
+        echo "Didn't find supported system."
+        exit 1
+    fi
+    echo -e "\nRun 'sudo $update_command'"
 }
 
 remove() {
-	echo "Repository removed from system."
+    echo "Repository removed from system."
 }
 
 if [ "$1" == "remove" ]; then
-	remove
+    remove
 else
-	install
+    install
 fi
