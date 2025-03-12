63b4aa0e3bf81622eb324304fcbb3a11a8d1050d
Lord J. Hackwell (niton) <lordhck@niton.dev>
2025-03-13 00:19:39 +0100

    even more tests arch.sh

--- arch.sh
+++ arch.sh
@@ -27,41 +27,41 @@ done
 
 format_disk() {
 
-	echo -e "
-	-------- Wiping Disk --------
-	"
-    parted --script "$DISK_SELECT" mklabel gpt || { echo "Failed to create partition table."; exit 1; }
+    echo -e "
+    -------- Wiping Disk --------
+    "
+    parted --script "$DISK_SELECT" mklabel gpt || true
 
-	echo -e "
-	-------- Creating EFI  --------
-	"
-    parted --script "$DISK_SELECT" mkpart ESP fat32 1MiB 513MiB
-    parted --script "$DISK_SELECT" set 1 esp on
+    echo -e "
+    -------- Creating EFI  --------
+    "
+    parted --script "$DISK_SELECT" mkpart ESP fat32 1MiB 513MiB || true
+    parted --script "$DISK_SELECT" set 1 esp on || true
 
     echo -e "
     -------- Creating Swap --------
     "
-    parted --script "$DISK_SELECT" mkpart primary linux-swap 513MiB 2561MiB
+    parted --script "$DISK_SELECT" mkpart primary linux-swap 513MiB 2561MiB || true
 
     echo -e "
     -------- Root Partition --------
     "
-    parted --script "$DISK_SELECT" mkpart primary ext4 2561MiB 100%
+    parted --script "$DISK_SELECT" mkpart primary ext4 2561MiB 100% || true
 
     echo -e "
-    -------- Formating Partition --------
+    -------- Formatting Partitions --------
     "
-    mkfs.fat -F32 "${DISK_SELECT}1"
-    mkswap "${DISK_SELECT}2"
-    mkfs.ext4 "${DISK_SELECT}3"
+    mkfs.fat -F32 -n EFI "${DISK_SELECT}1" || true
+    mkswap "${DISK_SELECT}2" || true
+    mkfs.ext4 -F "${DISK_SELECT}3" || true
 
     echo -e "
     -------- Mounting Partitions --------
     "
-    mount "${DISK_SELECT}3" /mnt
-    mkdir -p /mnt/boot
-    mount "${DISK_SELECT}1" /mnt/boot
-    swapon "${DISK_SELECT}2"
+    mount "${DISK_SELECT}3" /mnt || true
+    mkdir -p /mnt/boot || true
+    mount "${DISK_SELECT}1" /mnt/boot || true
+    swapon "${DISK_SELECT}2" || true
 
     echo -e "\nPartitioning and formatting completed."
 }
